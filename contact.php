2ab93d07e9f0e681a36ed665947bc71f943e2816
5ZYSZ3K <paw.por@wp.pl>
2020-03-20 13:34:01 +0100

    repairing the images

---
+++ contact.php
@@ -0,0 +1,12 @@
+<?php
+
+if(isset($_POST['submit'])){
+    $subject = $_POST['subject'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $mymail = 'paw.por@wp.pl';
+    $headers = "From: $email";
+    $txt = "You have received an email from $email.\n\n$message";
+    mail($mymail, $subject, $txt, $headers);
+    header("Location: index.html");
+}
